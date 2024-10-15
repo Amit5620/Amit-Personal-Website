8fc439dcf86fc9ec2fdf0201480535a25db41be7
Amit Kumar Mondal <mamit5620@gmail.com>
2024-10-16 00:42:31 +0530

    upload full project

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,13 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span> Amit Kumar Mondal B.Tech CSE | AI/ML Enthusiast | Data Science Intern | Research Analyst.\n I am a highly motivated B.Tech Computer Science and Engineering student from the 2025 passout batch with a strong focus on Artificial Intelligence and Machine Learning.\n My expertise spans across machine learning, deep learning, data analysis, explainable AI, and computer vision.\n I am proficient in Python, C++, and C, and possess solid knowledge of data structures and algorithms, alongside web technologies like HTML, CSS, JavaScript, and SQL.\n I have worked as an Assistant Research Analyst at CSIR Durgapur and a Data Science Intern at Celebal Technology.\n I have also demonstrated leadership skills as the AI/ML Lead for GDG BCET and contribute actively as a member of Code Vision and Cybercell. <span/>"
+        // ''' Amit Kumar Mondal B.Tech CSE | AI/ML Enthusiast | Data Science Intern | Research Analyst.
+        //   I am a highly motivated B.Tech Computer Science and Engineering student from the 2025 passout batch with a strong focus on Artificial Intelligence and Machine Learning.
+        //   My expertise spans across machine learning, deep learning, data analysis, explainable AI, and computer vision. I am proficient in Python, C++, and C, and possess solid knowledge of data structures and algorithms, alongside web technologies like HTML, CSS, JavaScript, and SQL.
+        //   I have worked as an Assistant Research Analyst at CSIR Durgapur and a Data Science Intern at Celebal Technology. I have also demonstrated leadership skills as the AI/ML Lead for GDG BCET and contribute actively as a member of Code Vision and Cybercell. '''
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 1000);
